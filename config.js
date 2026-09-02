094a737f2ca00d771e83f67caecafff33788ded4
dev-kaiops9 <devop9b@gmail.com>
2026-09-02 13:04:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,17 +18,17 @@ const MODULES = {
   station: {
     name: "RailStatiON",
     url: "https://railstation.kaiops9.com",
-    status: "27-08-2026"
+    status: "Segera Hadir"
   },
   imo: {
     name: "IMO Tools",
     url: "https://imo.kaiops9.com",
-    status: "27-08-2026"
+    status: "est. 27 Agustus 2026"
   },
   tracker: {
     name: "Tracker Kendala",
     url: "https://trackerkendala.kaiops9.com",
-    status: "28-08-2026"
+    status: "est. 10 Agustus 2026"
   },
   inovation: {
     name: "Inovation",
